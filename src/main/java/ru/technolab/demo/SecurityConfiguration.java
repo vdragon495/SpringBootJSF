fd72c9aaeafcacd5700616dcecfc875f2913f49c
Vladimir V. Vygodin <vygodinvv@idft.ru>
2019-10-12 01:06:15 +0300

    + security final auth

--- src/main/java/ru/technolab/demo/SecurityConfiguration.java
+++ src/main/java/ru/technolab/demo/SecurityConfiguration.java
@@ -1,12 +1,20 @@
 package ru.technolab.demo;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.GenericFilterBean;
 
 /** Конфигурация Spring Security через табличку testdb.users
  *  Пока чтобы её выключить нужно в configure() раскомментировать блок Permit all config:
@@ -16,27 +24,45 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+    @Autowired
+    private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
+ 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.inMemoryAuthentication()
+        	.withUser("user").password("{noop}111").roles("USER")
+        	.and()
+        	.withUser("admin").password("{noop}111").roles("ADMIN");
+    }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		// Permit all config:
-		http.cors().and()
-	    	.csrf()			// Cross Site Request Forgery (CSRF Attacks)
-	    	.disable()		// We don't need CSRF for this example
-	    	.authorizeRequests().antMatchers("/").permitAll();	// dont authenticate this particular request
-		
-		
 //		http.cors().and()
-//        	.csrf()			// Cross Site Request Forgery (CSRF Attacks)
-//        	.disable()		// We don't need CSRF for this example
-//        	.authorizeRequests().antMatchers("/graphiql", "/actuator/health").permitAll()	// dont authenticate this particular request
-//        	
-//
-//        	// all other requests need to be authenticated
-//        	.anyRequest().authenticated().and()
-//    		.addFilterBefore(jwtAuthenticationFilter(), 
-//    				UsernamePasswordAuthenticationFilter.class);
+//	    	.csrf()			// Cross Site Request Forgery (CSRF Attacks)
+//	    	.disable()		// We don't need CSRF for this example
+//	    	.authorizeRequests().antMatchers("/").permitAll();	// dont authenticate this particular request
+		
+		// secured
+        http
+	        .cors().and()
+	    	.csrf()			// Cross Site Request Forgery (CSRF Attacks)
+	    	.disable()		// We don't need CSRF for this example. Без этого PF нормально не работал!!!
+	        .authorizeRequests()
+	        .antMatchers("/").authenticated()
+	          .and()
+	          .httpBasic()
+	          .authenticationEntryPoint(authenticationEntryPoint);
+	 
+	        http.addFilterAfter(new GenericFilterBean() {
 
+				@Override
+				public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+						throws IOException, ServletException {
+					chain.doFilter(request, response);
+				}
+	        }, BasicAuthenticationFilter.class);
 	}
 }
